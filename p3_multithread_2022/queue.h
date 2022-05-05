a7ac003ac2938ffc28554f99251d1494994dba18
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2022-05-05 17:11:19 +0200

    Update queue.h

--- p3_multithread_2022/queue.h
+++ p3_multithread_2022/queue.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+ 
 
 struct element {
 	int type; //Machine type
@@ -16,11 +16,11 @@ typedef struct queue {
     struct element *array; // vector that forms the queue
 }queue;
 
-queue* queue_init (int size);
-int queue_destroy (queue *q);
-int queue_put (queue *q, struct element* elem);
-struct element * queue_get(queue *q);
-int queue_empty (queue *q);
-int queue_full(queue *q);
+queue*         queue_init (int size);
+int            queue_destroy (queue *q);
+int            queue_put (queue *q, struct element* elem);
+struct element queue_get (queue *q);
+int            queue_empty (queue *q);
+int            queue_full   (queue *q);
 
 #endif
