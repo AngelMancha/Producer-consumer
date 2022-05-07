bf64f8f9588787ec10b4ac5a68d16ac43335fce2
AngelMancha <98919482+AngelMancha@users.noreply.github.com>
2022-05-07 15:00:57 +0200

    Update queue.h

--- p3_multithread_2022/queue.h
+++ p3_multithread_2022/queue.h
@@ -19,7 +19,7 @@ typedef struct queue {
 queue*         queue_init (int size);
 int            queue_destroy (queue *q);
 int            queue_put (queue *q, struct element* elem);
-struct element queue_get (queue *q);
+struct element *queue_get (queue *q);
 int            queue_empty (queue *q);
 int            queue_full   (queue *q);
 
