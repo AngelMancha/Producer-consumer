1bdb77497902b4925c0e29a11a2d7cfe1c2774da
AngelMancha <98919482+AngelMancha@users.noreply.github.com>
2022-05-07 15:00:29 +0200

    Update queue.c

--- p3_multithread_2022/queue.c
+++ p3_multithread_2022/queue.c
@@ -9,10 +9,10 @@
 //To create a queue
 queue* queue_init(int size){
 	//Initialize the elements of the queue (initially all are 0)
-	queue * q = (queue *)malloc(sizeof(queue)); //q es una variable de tipo puntero // reservar memoria con el tamaño de la queue
+	queue * q = (queue *)malloc(sizeof(queue)); //q es una variable de tipo puntero // reservar memoria con el tamaÃ±o de la queue
 	q->size = size; //size of the queue
 	q->head = 0;
-	q->tail = 0; // q->tail = sizeof(queue) - 1;
+	q->tail = 0; //sizeof(q) - 1;
 	q -> capacity = 0; //number of elements in the queue
 	q -> array = malloc(q->size * sizeof(struct element)); // we consider the size of the element (time and type of machine)
 	return q;
@@ -65,36 +65,67 @@ struct element* queue_get(queue *q) {
 }
 */
 
+/*
 struct element queue_get(queue *q) {
         int ret ;
 	struct element element;
 
 	ret = queue_empty(q);
-        if (0 == ret)
+        if (1 == ret)
           return element;
 
-	memcpy(&element, &q -> array[q->head], sizeof(struct element));
+	memcpy(&element, &(q -> array[q->head]), sizeof(struct element));
 	//element = (q -> array[q->head]); // element = q.array[tail] we deque the first element in the queue (FIFO)
-//printf("\n\n EStamos en el queue.c y sacamos:%d\n", element.type);
+printf("\n\n EStamos en el queue.c y sacamos:%d\n", element.type);
 	q-> head = q->head + 1; //we increase by one the head
 	q->capacity = q->capacity -1; // the number of elements is decreased by one
 	return element;
+}*/
+
+
+
+
+
+
+struct element *queue_get(queue *q) {
+      struct element *element;
+      if (queue_empty(q) == 0) {
+            element = &(q->array[q->head]);
+        if (q->head+1 == q->size){
+            q->head=0;
+        }
+        else{
+            q->head = q->head+1;
+        }
+            q->capacity = q->capacity - 1;
+      }
+
+      return element;
 }
+
+
+
+
+
+
 
 //To check queue state
 int queue_empty(queue *q){
 	if (q->size != 0) {
 		printf("Queue not empty");
-		return 1; // return -1 in case the queue is empty
+		return 0; // return -1 in case the queue is empty
 	}
-	return 0;
+	else if (q -> size == 0){
+	return 1;
+	}
+	
 }
 
 int queue_full(queue *q){
 	// si size = sizeof(queue) q->size == q.size
 	if (q->size == q -> capacity ){
 		printf("Queue is full");
-		return 1; // return -1 in case the queue is full
+		return 1; // return 1 in case the queue is full
 	}
 	return 0;
 	
